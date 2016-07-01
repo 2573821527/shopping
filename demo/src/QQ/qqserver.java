a932784c7d53b6745bd5086c6c6ef7ce8125926b
2573821527 <2573821527@qq.com>
2016-07-01 18:15:00 +0800

    QQ

--- demo/src/QQ/qqserver.java
+++ demo/src/QQ/qqserver.java
@@ -1,97 +1,73 @@
 package QQ;
 
+import java.awt.Color;
 import java.awt.EventQueue;
-
-import javax.swing.JFrame;
-import javax.swing.JOptionPane;
-import javax.swing.JTextField;
-import javax.swing.JButton;
-import javax.swing.JFileChooser;
-
+import java.awt.Font;
+import java.awt.Point;
+import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.BufferedReader;
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.net.InetAddress;
+import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.UnknownHostException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.awt.event.ActionEvent;
-import java.awt.Color;
-import java.awt.Font;
-import java.awt.Image;
-import java.awt.Point;
-import java.awt.SystemColor;
-import javax.swing.SwingConstants;
-import javax.swing.text.html.ImageView;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
 import javax.swing.JTextArea;
+import javax.swing.SwingConstants;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
 import javax.swing.JLabel;
-import javax.imageio.ImageIO;
 import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
 
-public class qqtest extends Thread{
-	private static final String InetAdderss = null;
-	private static Socket socket;
-	private static JFrame frame;
+public class qqserver {
+    private static Socket socket;
+	private static  JFrame frame;
 	private static BufferedReader br;
 	private static JTextArea txt1;
 	private static JTextArea txt2;
+    private static ServerSocket ss;
 	/**
 	 * Launch the application.
 	 */
 	public static void main(String[] args) {
-		getConnection();
+		getConnetction();
 	}
 
 	/**
 	 * Create the application.
 	 */
-	public qqtest() {}
-
+	public qqserver(){}
 	/**
 	 * Initialize the contents of the frame.
 	 */
 	private static  void initialize() {
 		frame = new JFrame();
 		frame.setTitle("\u804A\u5929\u7A0B\u5E8F");
-		frame.getContentPane().setBackground(new Color(255,255,255));
-		frame.setBounds(100, 100, 495, 510);
+		frame.getContentPane().setBackground(new Color(255, 255, 255));
+		frame.setBounds(100, 100, 499,527);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.getContentPane().setLayout(null);
-		
-		JTextArea txtrS = new JTextArea();
-		txtrS.setText("s");
-		txtrS.setBounds(-3, 259, 4, 24);
-		frame.getContentPane().add(txtrS);
+        frame.getContentPane().setLayout(null);
 		
 		txt1 = new JTextArea();
-		txt1.setBounds(-3, 340, 346, 110);
+		txt1.setBounds(0, 356, 346, 109);
+		txt1.setBorder(BorderFactory.createTitledBorder(""));
 		frame.getContentPane().add(txt1);
-		txt1.setBackground(new Color(255,255,255));
-        txt1.setBorder(BorderFactory.createTitledBorder(""));
 
 		
 		txt2 = new JTextArea();
 		txt2.setEditable(false);
-		txt2.setBounds(-3, 63, 346, 249);
-		txt2.setBackground(new Color(255,255,255));
+		txt2.setBounds(0, 61, 346, 266);
 		txt2.setBorder(BorderFactory.createTitledBorder(""));
 		frame.getContentPane().add(txt2);
-		
-		JTextArea textArea = new JTextArea();
-		textArea.setBounds(-3, 259, 4, 24);
-		frame.getContentPane().add(textArea);
-		
+
 		JButton close = new JButton("关闭");
 		close.addActionListener(new ActionListener() {
 			
@@ -101,8 +77,8 @@ public class qqtest extends Thread{
 				System.exit(0);
 			}
 		});
-		close.setBackground(new Color(250,240,230));
-		close.setBounds(188, 448, 70, 24);
+		close.setBackground(new Color(250, 240, 230));
+		close.setBounds(196, 466, 70, 23);
 		frame.getContentPane().add(close);
 		
 		JButton send = new JButton("发送");
@@ -111,7 +87,7 @@ public class qqtest extends Thread{
 						if(!(txt1.getText()==null)&&!"".equals(txt1.getText().trim())){//检查textID的文本是不是全部为空格或者干脆就是空字符串,trim()是用来去掉字符串开头和尾巴上的所以空格的。
 							PrintWriter pw=null;
 							Date date=new Date(System.currentTimeMillis());
-							SimpleDateFormat sdf=new SimpleDateFormat("yyyy-mm-dd HH:MM:SS E");
+							SimpleDateFormat sdf=new SimpleDateFormat("yyyy-mm-dd HH:MM:SS");
 							txt2.setCaretPosition(txt2.getText().length());//举例说明：jTextArea.setCaretPosition(jTextArea.getDocument().getLength());
                                                                            //以上是将滚动条自动拉到JTextArea最底端，这样就可以看到最低端的数据了。
                                                                            //jTextArea.setCaretPosition(0);
@@ -140,7 +116,7 @@ public class qqtest extends Thread{
 			}
 		});
 
-		send.setBounds(273, 449, 70, 23);
+		send.setBounds(276, 467, 70, 23);
 		send.setBackground(new Color(250,240,230));
 		frame.getContentPane().add(send);
 		
@@ -148,7 +124,7 @@ public class qqtest extends Thread{
 		btnNewButton_2.setBackground(new Color(255, 255, 255));
 		btnNewButton_2.setBorder(null);
 		btnNewButton_2.setFont(new Font("宋体", Font.BOLD | Font.ITALIC, 17));
-		btnNewButton_2.setToolTipText("字体选择工具栏");
+		btnNewButton_2.setToolTipText("\u5B57\u4F53\u9009\u62E9\u5DE5\u5177\u680F");
 		btnNewButton_2.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				//btnNewButton_2.setBorder(null);
@@ -157,19 +133,18 @@ public class qqtest extends Thread{
 		});
 		//btnNewButton_2.setBackground(Color.LIGHT_GRAY);
 		btnNewButton_2.setForeground(Color.BLACK);
-		btnNewButton_2.setBounds(-3, 313, 35, 24);
+		btnNewButton_2.setBounds(0, 327, 35, 29);
 		frame.getContentPane().add(btnNewButton_2);
 		
 		JLabel lblNewLabel = new JLabel("New label");
 		lblNewLabel.setIcon(new ImageIcon("D:\\javaproject\\demo\\images\\QQ\u622A\u56FE20160701145016.png"));
-		lblNewLabel.setBounds(0, 0, 61, 62);
+		lblNewLabel.setBounds(0, 0, 60, 58);
 		frame.getContentPane().add(lblNewLabel);
 		
 		JLabel lblNewLabel_1 = new JLabel("New label");
 		lblNewLabel_1.setIcon(new ImageIcon("D:\\javaproject\\demo\\images\\QQ\u622A\u56FE20160701150525.png"));
-		lblNewLabel_1.setBounds(343, 63, 136, 409);
+		lblNewLabel_1.setBounds(346, 56, 138, 433);
 		frame.getContentPane().add(lblNewLabel_1);
-		
 		
 		JButton btnNewButton = new JButton("",new ImageIcon("images/3.png"));
 		btnNewButton.setVerticalAlignment(SwingConstants.BOTTOM);
@@ -179,7 +154,7 @@ public class qqtest extends Thread{
 			public void actionPerformed(ActionEvent e) {
 			}
 		});
-		btnNewButton.setBounds(39, 315, 35, 24);
+		btnNewButton.setBounds(41, 326, 35, 30);
 		frame.getContentPane().add(btnNewButton);
 		
 		JButton button = new JButton("",new ImageIcon("images/2.png"));
@@ -202,77 +177,65 @@ public class qqtest extends Thread{
 			}
 		});
 		button.setContentAreaFilled(false);
-		button.setBounds(76,312,35,30);
+		button.setBounds(86,328,35,30);
 		button.setBorder(null);
 		frame.getContentPane().add(button);
+		frame.setBackground(new Color(255,255,255));
 		
 		JButton btnNewButton_1 = new JButton("New button",new ImageIcon("images/4.png"));
 		btnNewButton_1.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				JFileChooser jfc=new JFileChooser();				
-				File file=jfc.getSelectedFile();
-				String open=file.getAbsolutePath();
-				System.out.println(open);
-/*                try {
-					FileInputStream fis=new FileInputStream(file);
-					byte[] sends=new byte[1024];
-					
-				} catch (FileNotFoundException e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
-				}*/
-                
+				
 			}
 		});
-		btnNewButton_1.setBounds(127, 311, 32, 30);
+		btnNewButton_1.setBounds(131, 327, 32, 30);
 		btnNewButton_1.setBorder(null);
 		btnNewButton_1.setContentAreaFilled(false);
 		frame.getContentPane().add(btnNewButton_1);
         frame.setVisible(true);
 	}
-	private static  void getConnection(){
-
-			try {
-				initialize();
-				while(true){
-				socket=new Socket(InetAddress.getLocalHost(),9000);
+    private static  void getConnetction(){
+    	try {
+			ss=new ServerSocket(9000, 10, InetAddress.getLocalHost());			
+			initialize();		
+			while(true){
+				socket=ss.accept();
 				socket.setKeepAlive(true);
-				 System.out.println("连接服务器:" + socket.getRemoteSocketAddress() + "成功！");
-				//client.setKeepAlive(true);
+				System.out.println("连接服务器:" + socket.getRemoteSocketAddress() + "成功！");
 				getMessage();
-				}
-			} catch (UnknownHostException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+				
 			}
-	}
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    }
 
-	private static void getMessage() {
+	private static  void getMessage() {
 		// TODO Auto-generated method stub
 		String temp=new String();
 		try {
 			br=new BufferedReader(new InputStreamReader(socket.getInputStream()));
-			while(true){
-			temp=br.readLine();
-			if(temp==null){
-				break;
-			}
-			Date date=new Date(System.currentTimeMillis());
-			SimpleDateFormat sdf=new SimpleDateFormat("yyy-mm-dd HH:MM:SS E");
-			txt2.setCaretPosition(txt2.getText().length());
-			if("".equals(txt2.getText().trim())){
-				txt2.append("\n你"+sdf.format(date)+"\n"+temp);
-			}
-			else{
-				txt2.append("\n你"+sdf.format(date)+"\n"+temp);
-			}
+			while(!socket.isClosed()){
+				temp=br.readLine();
+				if(temp==null){
+					break;
+				}
+				Date date=new Date(System.currentTimeMillis());
+				SimpleDateFormat sdf=new SimpleDateFormat("yyyy-mm-dd HH:MM:SS");
+				if(txt2.getText()==null||"".equals(txt2.getText().trim())){
+					txt2.append("\n你"+sdf.format(date)+"\n"+temp);
+				}else{
+					txt2.append("\n你"+sdf.format(date)+"\n"+temp);
+				}
+				
 			}
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-	}
+	} 
 }
